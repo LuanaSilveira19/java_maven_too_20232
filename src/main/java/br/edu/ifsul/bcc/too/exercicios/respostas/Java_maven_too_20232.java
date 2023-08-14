3c34f1df046f4fde50c611c0936643a89a661bdf
Loja <Loja@172.16.219.116>
2023-08-14 09:52:58 -0300

    Alteracao da aula de 14/08/2023

--- src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_20232.java
+++ src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/Java_maven_too_20232.java
@@ -10,6 +10,12 @@ package br.edu.ifsul.bcc.too.exercicios.respostas;
  */
 public class Java_maven_too_20232 {
 
+    
+    
+    
+    
+    
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
